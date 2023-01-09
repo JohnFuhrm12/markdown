84b86b1d42d731c058dc1123f2893b681e479415
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-09 16:40:40 -0300

    Parses Markdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import {marked} from 'marked';
 
 function App() {
+  const initialText = `
+  # Welcome to my React Markdown Previewer!
+  
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:`
+
+  const [text, setText] = useState(initialText);
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    setMarkdown(marked.parse(text));
+  })
+
   return (
     <>
     <div className="container">
       <div className='sectionContainer'>
         <h2 className='title' id='editorTitle'>Editor</h2>
-        <textarea id='editor'/>
+        <textarea value={text} onChange={(e) => {setText(e.target.value)}} id='editor'/>
       </div>
       <div className='sectionContainer'>
         <h2 className='title'>Preview</h2>
-        <div id='preview'/>
+        <div id='preview' dangerouslySetInnerHTML={{__html: markdown}}></div>
       </div>
     </div>
     </>
