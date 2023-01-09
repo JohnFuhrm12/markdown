47da167dec3f781d25f0f4852b21d7212b85519d
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-09 16:57:26 -0300

    Removed Tests

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import { useEffect, useState } from 'react';
 import {marked} from 'marked';
 
 function App() {
-  const initialText = `
-  # Welcome to my React Markdown Previewer!
-  
+  const initialText = 
+  `# Welcome to my React Markdown Previewer!
+
   ## This is a sub-heading...
-  ### And here's some other cool stuff:`
+  ### And here's some other cool stuff:
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
 
   const [text, setText] = useState(initialText);
   const [markdown, setMarkdown] = useState('');
@@ -16,6 +43,10 @@ function App() {
   useEffect(() => {
     setMarkdown(marked.parse(text));
   })
+
+  useEffect(() => {
+    document.getElementById('preview').innerHTML = markdown;
+  }, [markdown])
 
   return (
     <>
@@ -26,7 +57,7 @@ function App() {
       </div>
       <div className='sectionContainer'>
         <h2 className='title'>Preview</h2>
-        <div id='preview' dangerouslySetInnerHTML={{__html: markdown}}></div>
+        <div id='preview'></div>
       </div>
     </div>
     </>
